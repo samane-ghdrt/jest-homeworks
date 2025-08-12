04c3480cef1b54b53b0905fcdbf8e33838a97a19
samane <samane.ghodrati@gmail.com>
2025-08-12 02:30:10 -0700

    add

--- jest-homeworks/0-Beginner/2-sum2.test.js
+++ jest-homeworks/0-Beginner/2-sum2.test.js
@@ -2,3 +2,9 @@ const sum=require('./2-sum2')
 test('test1',()=>{
 expect(sum(1,2)).toBe(3)
 })
+test('test1',()=>{
+expect(sum(-1,3)).toBe(2)
+})
+test('test1',()=>{
+expect(sum(1,"e")).toBeFalsy()
+})
